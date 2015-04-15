4cda8f6f2ad1535497d7d6d6b8b8f0e4f5250756
Sarah GP <sarah.groff.palermo@gmail.com>
2015-04-15 13:28:50 -0400

    Happy with build; drawing purple rect

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp    = require('gulp'),
     stylus  = require('gulp-stylus'),
-    eslint  = require('gulp-eslint');
+    eslint  = require('gulp-eslint'),
+    nib     = require('nib');
 
 gulp.task('compile-css', function(){
   gulp.src('./src/css/*.styl')
-      .pipe(stylus())
+      .pipe(stylus({ use: nib(), import: ['nib']}))
       .pipe(gulp.dest('./build/css'))
+});
+
+gulp.task('compile-html', function(){
+  // currently just copying over the html; can add handlebars if necc later
+  gulp.src('./src/*.html')
+      .pipe(gulp.dest('./build'))
 });
 
 gulp.task('lint', function(){
@@ -15,5 +22,7 @@ gulp.task('lint', function(){
 });
 
 gulp.task('automate', function(){
-  gulp.watch('*.styl', ['compile-css'])
+  gulp.watch(['./src/css/*.styl', './src/*.html'], ['compile-css', 'compile-html']);
 });
+
+gulp.task('build', ['compile-css', 'compile-html']);
