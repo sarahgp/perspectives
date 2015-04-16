dcc0b1b219d0f871a39cbc731a2ce95f44c5cbd7
Sarah GP <sarah.groff.palermo@gmail.com>
2015-04-16 14:10:01 -0400

    using skeleton; draw it through via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
-var gulp    = require('gulp'),
-    stylus  = require('gulp-stylus'),
-    eslint  = require('gulp-eslint'),
-    nib     = require('nib');
+var gulp      = require('gulp'),
+    stylus    = require('gulp-stylus'),
+    eslint    = require('gulp-eslint'),
+    nib       = require('nib'),
+    concat    = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    rename    = require('gulp-rename');
 
-gulp.task('compile-css', function(){
+gulp.task('compile-concat-minify-css', function(){
   gulp.src('./src/css/*.styl')
       .pipe(stylus({ use: nib(), import: ['nib']}))
+      .pipe(gulp.dest('./build/css'));
+
+  // For vanilla version, using skeleton as a framework
+  // Change / update this task to control what is imported into build
+  gulp.src('./src/css/vendor/**/*')
+      .pipe(gulp.dest('./build/css'));
+
+  gulp.src('./build/css/**/*')
+      .pipe(concat('style.css'))
       .pipe(gulp.dest('./build/css'))
+      .pipe(minifyCSS())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('compile-html', function(){
@@ -19,7 +34,7 @@ gulp.task('copy-fetch', function(){
   // just move fetch from node-modules, so it can stay up-to-date
   gulp.src('./node_modules/whatwg-fetch/fetch.js')
       .pipe(gulp.dest('./build/js'));
-})
+});
 
 gulp.task('lint', function(){
   gulp.src('./src/js/*.js')
@@ -29,7 +44,7 @@ gulp.task('lint', function(){
 });
 
 gulp.task('automate', function(){
-  gulp.watch(['./src/css/*.styl', './src/*.html'], ['compile-css', 'compile-html']);
+  gulp.watch(['./src/css/*.styl', './src/*.html'], ['compile-concat-minify-css', 'compile-html']);
 });
 
-gulp.task('build', ['compile-css', 'compile-html', 'copy-fetch']);
+gulp.task('build', ['compile-concat-minify-css', 'compile-html', 'copy-fetch']);
