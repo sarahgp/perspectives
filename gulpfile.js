c284c5e83b5ade01377bc9f526d5cd40166bb5f8
Sarah GP <sarah.groff.palermo@gmail.com>
2015-04-18 10:47:19 -0400

    Import skeleton as stylus file to avoid annoying sequencing

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,11 @@
 var gulp      = require('gulp'),
     stylus    = require('gulp-stylus'),
     eslint    = require('gulp-eslint'),
-    nib       = require('nib'),
-    concat    = require('gulp-concat'),
-    minifyCSS = require('gulp-minify-css'),
-    rename    = require('gulp-rename');
+    nib       = require('nib');
 
-gulp.task('compile-concat-minify-css', function(){
+gulp.task('compile-css', function(){
   gulp.src('./src/css/*.styl')
       .pipe(stylus({ use: nib(), import: ['nib']}))
-      .pipe(gulp.dest('./build/css'));
-
-  // For vanilla version, using skeleton as a framework
-  // Change / update this task to control what is imported into build
-  gulp.src('./src/css/vendor/**/*')
-      .pipe(gulp.dest('./build/css'));
-
-  gulp.src('./build/css/**/*')
-      .pipe(concat('style.css'))
-      .pipe(gulp.dest('./build/css'))
-      .pipe(minifyCSS())
-      .pipe(rename('style.min.css'))
       .pipe(gulp.dest('./build/css'));
 });
 
@@ -44,7 +29,7 @@ gulp.task('lint', function(){
 });
 
 gulp.task('automate', function(){
-  gulp.watch(['./src/css/*.styl', './src/*.html'], ['compile-concat-minify-css', 'compile-html']);
+  gulp.watch(['./src/css/*.styl', './src/*.html'], ['compile-css', 'compile-html']);
 });
 
-gulp.task('build', ['compile-concat-minify-css', 'compile-html', 'copy-fetch']);
+gulp.task('build', ['compile-css', 'compile-html', 'copy-fetch']);
