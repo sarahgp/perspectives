2f1126a29704f366358559cfcc3fe9895d016a0f
Sarah GP <sarah.groff.palermo@gmail.com>
2015-04-20 16:40:55 -0400

    Janky vanilla

--- build/js/vanilla-data.js
+++ build/js/vanilla-data.js
@@ -1,22 +1,33 @@
 console.log('vanilla!');
 
-var look = document.getElementById('look');
+var look  = document.getElementById('look'),
+    input = document.getElementsByTagName("input");
 
 look.onsubmit = function(){
   event.preventDefault();
-  console.log('Submit!');
+  console.log('Submit!', input.lookFor.value);
+  var val = input.lookFor.value.split(':');
+  fetch('http://0.0.0.0:3000/books/books/')
+    .then(function(response) {
+      console.log(val);
+      if (response.status !== 200) {  
+              console.log('Looks like there was a problem. Status Code: ' +  
+                response.status);  
+              return;  
+      }
+      return response.json();
+    }).then(function(json) {
+      var words = document.getElementById('words');
+      val[1] = val[1].trim();
+      console.log(val);
+      json.forEach(function(el){
+        if (el[val[0]] === val[1] || el[val[0]].toLowerCase() === val[1]){
+          var p = document.createElement("p");
+          p.innerHTML = JSON.stringify(el.color);
+          words.appendChild(p);
+        }
+      })
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+    });
 }
-
-// fetch('http://0.0.0.0:3000/books/books')
-//   .then(function(response) {
-//     return response.json()
-//   }).then(function(json) {
-//     var words = document.getElementById('words');
-//     json.forEach(function(el){
-//       var p = document.createElement("p");
-//       p.innerHTML = JSON.stringify(el.color);
-//       words.appendChild(p);
-//     })
-//   }).catch(function(ex) {
-//     console.log('parsing failed', ex)
-//   });
