7e8160c4b0c1dcf5a08e17ab2ce383d707e6a3ba
Sarah GP <sarah.groff.palermo@gmail.com>
2015-04-20 13:44:45 -0400

    Sketching form

--- build/js/vanilla-data.js
+++ build/js/vanilla-data.js
@@ -1,15 +1,22 @@
 console.log('vanilla!');
 
-fetch('http://0.0.0.0:3000/books/books')
-  .then(function(response) {
-    return response.json()
-  }).then(function(json) {
-    var words = document.getElementById('words');
-    json.forEach(function(el){
-      var p = document.createElement("p");
-      p.innerHTML = JSON.stringify(el.color);
-      words.appendChild(p);
-    })
-  }).catch(function(ex) {
-    console.log('parsing failed', ex)
-  });
+var look = document.getElementById('look');
+
+look.onsubmit = function(){
+  event.preventDefault();
+  console.log('Submit!');
+}
+
+// fetch('http://0.0.0.0:3000/books/books')
+//   .then(function(response) {
+//     return response.json()
+//   }).then(function(json) {
+//     var words = document.getElementById('words');
+//     json.forEach(function(el){
+//       var p = document.createElement("p");
+//       p.innerHTML = JSON.stringify(el.color);
+//       words.appendChild(p);
+//     })
+//   }).catch(function(ex) {
+//     console.log('parsing failed', ex)
+//   });
