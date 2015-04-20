b26890834b697027654b916ae67a5dffa8393457
Sarah GP <sarah.groff.palermo@gmail.com>
2015-04-20 17:28:24 -0400

    Draws table

--- src/js/vanilla-data.js
+++ src/js/vanilla-data.js
@@ -1,10 +1,12 @@
 console.log('vanilla!');
 
 var look  = document.getElementById('look'),
+    words = document.getElementById('words'),
     input = document.getElementsByTagName("input");
 
 look.onsubmit = function(){
   event.preventDefault();
+  words.innerHTML = '';
   console.log('Submit!', input.lookFor.value);
   var val = input.lookFor.value.split(':');
   fetch('http://0.0.0.0:3000/books/books/')
@@ -17,13 +19,27 @@ look.onsubmit = function(){
       }
       return response.json();
     }).then(function(json) {
-      var words = document.getElementById('words');
+      var keys  = _.pull(Object.keys(json[0]), '_id');
+
+      keys.forEach(function(el){
+          var th = document.createElement("th")
+          th.innerHTML = el;
+          words.appendChild(th);
+      });
+
       val[1] = val[1].trim();
+
       json.forEach(function(el){
         if (el[val[0]] === val[1] || el[val[0]].toLowerCase() === val[1]){
-          var p = document.createElement("p");
-          p.innerHTML = JSON.stringify(el.color);
-          words.appendChild(p);
+          var tr = document.createElement("tr");
+
+          keys.forEach(function(key){
+            var td = document.createElement("td");
+            td.innerHTML = el[key];
+            tr.appendChild(td);
+          });
+
+          words.appendChild(tr);
         }
       })
     }).catch(function(ex) {
